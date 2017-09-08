1f94fbe258f629632674d808008392672dc3b11d
Aziraphale <aziraphale0108@gmail.com>
2017-09-08 16:18:20 +1000

    webpages with bootstrap

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//menu buttom collpse
+$(function(){
+	$("#navbar-toggle").blur(function(event){
+		var screenWidth = window.innerWidth;
+		if (screenWidth < 768) {
+			$("#collapsable-nav").collapse('hide');
+		}
+	});
+});
